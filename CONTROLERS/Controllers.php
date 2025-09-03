6d473c71b482527f286c4cdbd6eaac54e2bab03c
Lucas Souza e Silva <256582025@eniac.edu.br>
2025-09-03 03:18:47 -0300

    first commit

---
+++ CONTROLERS/Controllers.php
@@ -0,0 +1,4 @@
+    <?php
+    // Esta é uma linha de comentário
+    echo "Olá, mundo!"; // Isto não será executado
+    ?>
---
+++ JS/index.js
@@ -0,0 +1,8 @@
+/*
+Este é um comentário
+que ocupa
+várias linhas.
+É útil para explicações longas
+ou para desativar temporariamente um bloco de código.
+*/
+let y = 20;
